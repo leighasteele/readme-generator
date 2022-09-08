811a0605aed52fabc922ea96edcd6dc9b2115233
Bambi <leighasteele10@gmail.com>
2022-09-08 17:19:08 -0400

    wrote first part of index.js

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
+const questions = inquirer.createQuestionModule();
 // TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+prompt([
+  {
+    name: "title",
+    message: "Enter a title for your README.",
+  },
+  {
+    name: "description",
+    message: "Enter a description for your README.",
+  },
+  {
+    name: "installation",
+    message: "Enter an installation process.",
+  },
+  {
+    name: "usage",
+    message: "Enter usage instructions.",
+  },
+  {
+    name: "license",
+    message: "Enter a license.",
+  },
+  {
+    name: "contribution",
+    message: "Enter contribution guidelines.",
+  },
+  {
+    name: "tests",
+    message: "Enter any tests.",
+  },
+  {
+    name: "questions",
+    message: "Enter contact information.",
+  },
+]).then((answers) => {
+  console.log(answers);
+  const template = `
+    ${answers.title}
+    ${answers.description}
+    ${answers.installation}
+    ${answers.usage}
+    ${answers.license}
+    ${answers.contriibution}
+    ${answers.tests}
+    ${answers.questions}
+    `;
+  if (fs.existsSync("output")) {
+    fs.writeFileSync("output/product.md", template);
+  } else {
+    fs.mkdirSync("output");
+    fs.writeFileSync("output/product.md", template);
+  }
+});
---
+++ template.md
@@ -0,0 +1,18 @@
+# README.md Generator
+
+## Table of Contents
+[Description](#description)
+
+[Visuals](#visuals)
+
+## Description
+
+[Link to live site](https://leighasteele.github.io/readme-generator/)
+
+Lorem ipsum 
+
+
+## Visuals
+
+![Screenshot of terminal]()
+![Screenshot of product]()
